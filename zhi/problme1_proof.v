1d2fd3d0b2073633c2eafeb367e9aa7ed4b9b0c8
Zhi Zhang <zhangzhi@ksu.edu>
2014-06-15 11:00:54 +0200

    prove the first lemma with one assumption still needs to be proved

--- zhi/problme1_proof.v
+++ zhi/problme1_proof.v
@@ -56,13 +56,14 @@ Inductive PatienceSolitaire_Rule: list (Index * nat) -> Stacks -> Stacks -> Prop
 
 
 
-(********************************************************************************
-   < Lemma 1 >
-   
-   To each element in stack i+ 1, there is an element in stack i that precedes it, 
-   so that by chaining these elements, we can construct an increasing subsequence 
-   of length n;
- ********************************************************************************)
+
+
+
+
+
+
+
+
 
 Require Import Coq.Init.Datatypes.
 
@@ -96,14 +97,24 @@ Section Auxiliary.
         i < i' ->
         Index_Increasing_True ((i', n') :: ls) ->
         Index_Increasing_True ((i, n) :: (i', n') :: ls).
+
+  Lemma Index_Increasing_True_Subset: forall a ls,
+    Index_Increasing_True (a :: ls) ->
+      Index_Increasing_True ls.
+  Proof.
+    intros.
+    inversion H; subst;
+    smack.
+    constructor.
+  Qed.
   
   (* Stack_Indexes_Lt stack i: means all indexes of values in stack are less
      than i; 
   *)
   Inductive Stack_Indexes_Lt: Stack -> nat -> Prop :=
-    | S_Less_Than1: forall i' i stack n n',
+    | S_Less_Than1: forall i' i stack n',
         i' < i ->
-        Stack_Indexes_Lt stack n ->
+        Stack_Indexes_Lt stack i ->
         Stack_Indexes_Lt ((i', n') :: stack) i
     | S_Less_Than2: forall i,
         Stack_Indexes_Lt nil i.
@@ -119,6 +130,14 @@ Section Auxiliary.
     | Less_Than2: forall i,
         Stacks_Indexes_Lt nil i.
 
+  Lemma Stack_Indexes_Lt_Subset: forall i' n' ls i,
+    Stack_Indexes_Lt ((i', n') :: ls) i ->
+    i' < i /\ Stack_Indexes_Lt ls i.
+  Proof.
+    intros.
+    inversion H; subst; smack.
+  Qed.
+
   Lemma Stacks_Indexes_Lt_Subset: forall stack stacks i,
     Stacks_Indexes_Lt (stack :: stacks) i ->
     Stack_Indexes_Lt stack i /\ Stacks_Indexes_Lt stacks i.
@@ -126,6 +145,56 @@ Section Auxiliary.
     intros.
     inversion H; subst; smack.
   Qed.  
+
+
+  Lemma Stack_Indexes_Lt_Sn: forall stack i i',
+    Stack_Indexes_Lt stack i ->
+    i < i' ->
+    Stack_Indexes_Lt stack i'.
+  Proof.
+    intros; induction H; smack.
+    constructor; smack.
+    constructor.
+  Qed.
+
+  Lemma Stacks_Indexes_Lt_Sn: forall stacks i i',
+    Stacks_Indexes_Lt stacks i ->
+    i < i' ->
+    Stacks_Indexes_Lt stacks i'.
+  Proof.
+    intros; induction H; smack.
+    constructor; smack.
+    apply Stack_Indexes_Lt_Sn with (i := i); smack.
+    constructor.
+  Qed.
+ 
+
+  Lemma Stacks_Indexes_Lt_Trans: forall i n stacks stacks' i',
+    Insert_Rule (i, n) stacks stacks' ->
+    Stacks_Indexes_Lt stacks i ->
+    i < i' ->
+    Stacks_Indexes_Lt stacks' i'.
+  Proof.
+    intros i n stacks stacks' i' H. revert i'.
+    remember (i, n).
+    induction H. rewrite Heqp.
+    - smack.
+      inversion H1; subst.
+      constructor; smack.
+      constructor; smack.
+      apply Stack_Indexes_Lt_Sn with (i := i); smack.
+      apply Stacks_Indexes_Lt_Sn with (i := i); smack.
+    - smack.
+      inversion H3; subst.
+      specialize (H2 _ H8 H4).
+      constructor; smack.
+      apply Stack_Indexes_Lt_Sn with (i := i); smack.
+    - rewrite Heqp.
+      smack.
+      constructor; constructor; smack.
+      constructor.
+  Qed.
+
   
   (* Precede stack stack': for each element in stack', there is an element
      in stack that precedes it; 
@@ -154,7 +223,7 @@ Section Auxiliary.
     induction H; smack.
     constructor.
     exists x, x0.
-    specialize (In_Supperset (Index * nat) (x, x0) (a0, b) stack H1).
+    specialize (In_Supperset _ (x, x0) (a0, b) stack H1).
     smack.
     apply IHPrecede.
     constructor.
@@ -177,32 +246,51 @@ Section Auxiliary.
   Qed.
 
 
+  Lemma Precede_Reserved_Help1: forall i n stacks stacks1 i0 n0 stack0,
+    Insert_Rule (i, n) stacks stacks1 ->
+    i > i0 ->
+    n > n0 ->
+    Precede ((i0, n0) :: stack0) (nth 0 stacks default) ->
+    Precede ((i0, n0) :: stack0) (nth 0 stacks1 default).
+  Proof.
+    intros.
+    inversion H; subst; smack.
+  - constructor.
+    exists i0, n0; smack.
+    auto.
+  - constructor.
+    exists i0, n0; smack.
+    constructor.
+  Qed.
+
+
   (* if the stacks has "Precede" property, then after inserting an element
      a into the stack according to Patience Solitaire rules, then the result
      stack' should also reserve the "Precede" property.
   *)
-  Lemma Precede_Reserved: forall a stacks stacks' i,
-    Insert_Rule a stacks stacks' ->
-    Stacks_Indexes_Lt stacks (Index_Of a) ->
+  Lemma Precede_Reserved: forall a stacks stacks',
+    Insert_Rule a stacks stacks' -> (*  put element a into the stacks according to Patience Solitaire rule *)
+    Stacks_Indexes_Lt stacks (Index_Of a) -> (* index of a should be greater than indexes of elements on stacks *)
     (forall j, j >= 1 -> j <= ((length stacks) - 1) -> 
                Precede (nth (j-1) stacks default) (nth j stacks default)) ->  
-    i >= 1 ->
-    i <= ((length stacks') - 1) ->
-    Precede (nth (i - 1) stacks' default) (nth i stacks' default).
+    (forall i, i >= 1 -> i <= ((length stacks') - 1) ->
+               Precede (nth (i - 1) stacks' default) (nth i stacks' default)).
   Proof.
 (*
     intros. 
     destruct H; smack.
-    specialize (H0 i H1 H2).
+    specialize (H1 i H2 H3).
     destruct i; smack.
     destruct i; smack.
     apply Add_Reserved; auto.
-    specialize (Insert_Rule_Property (i0, n) stacks stacks' H4).
+    specialize (Insert_Rule_Property (i0, n) stacks stacks' H5).
     smack.
-    rewrite H5 in *.
-    specialize (H0 i H1 H2).
+    rewrite H6 in *.
+    specialize (H1 i H2 H3).
     destruct i; smack.
     destruct i; smack.
+    admit.
+    admit. 
 *)
 (*
     intros a stacks stacks' i H. revert i.
@@ -226,7 +314,8 @@ Section Auxiliary.
     destruct i0; smack.    
 *)    
 
-    intros a stacks stacks' i H. revert i.
+(*
+    intros a stacks stacks' H.
     induction H; smack.
   - specialize (H2 i0 H3 H4).
     destruct i0; smack.
@@ -238,15 +327,34 @@ Section Auxiliary.
     specialize (H3 i0 H4 H5).
     destruct i0; smack.
     destruct i0; smack.
-    + admit. (* ######### *)
+    + specialize (Stacks_Indexes_Lt_Subset _ _ _ H2); smack.
+      specialize (Stack_Indexes_Lt_Subset _ _ _ _ H7); smack.
+      apply Precede_Reserved_Help1 with (i := i) (n := n) (stacks := stacks);
+      smack.
+      specialize (leb_complete_conv _ _ H0).
+      smack.
+    + 
+      assert(HA: (forall j : nat,
+                   j >= 1 ->
+                   j <= length stacks - 1 ->
+                   Precede (nth (j - 1) stacks default)
+                     (nth j stacks default))).
       
-
-
       
-    + remember (S i0) as v.
+      
+      remember (S i0) as v.
       assert(i0 = v -1) as HA1. omega.
-      rewrite HA1 in *.      
+      rewrite HA1 in *.
+      assert (HA2: Stacks_Indexes_Lt stacks i).
+      specialize (Stacks_Indexes_Lt_Subset _ _ _ H2); smack.      
+      specialize (IHInsert_Rule v HA2).
+      
+      assert (HA3: v >= 1). admit.      
+      assert (HA4: v >= 1). admit.
+      
+      
       apply IHInsert_Rule; smack.
+      specialize (Stacks_Indexes_Lt_Subset _ _ _ H2); smack.
       destruct v; smack.
       remember (S v) as k.
       assert(v - 0 = k - 1) as HA1. omega.
@@ -255,7 +363,8 @@ Section Auxiliary.
       inversion H8.
       admit.
     + admit.
-      
+*)
+    admit.
   Qed.
 
  
@@ -263,77 +372,62 @@ End Auxiliary.
 
 
 
-Lemma lemma1: forall cards InitialStacks ResultStacks i,
-  PatienceSolitaire_Rule cards InitialStacks ResultStacks ->
+Lemma lemma1_helper: forall cards i card InitialStacks ResultStacks,
+  PatienceSolitaire_Rule ((i, card) :: cards) InitialStacks ResultStacks ->
+  Index_Increasing_True ((i, card) :: cards) ->
+  Stacks_Indexes_Lt InitialStacks i ->
   (* requirement for the initial stack *)
   (forall j, j >= 1 -> j <= ((length InitialStacks) - 1) -> 
              Precede (nth (j-1) InitialStacks default) (nth j InitialStacks default)) ->
-  i >= 1 -> 
-  i <= (length ResultStacks - 1) ->
-  Precede (nth (i-1) ResultStacks default) (nth i ResultStacks default).
+  (forall i, i >= 1 -> 
+             i <= (length ResultStacks - 1) ->
+             Precede (nth (i-1) ResultStacks default) (nth i ResultStacks default)).
 Proof.
   induction cards.
 - intros.
   inversion H; subst.
-  specialize (H0 i H1 H2).
+  inversion H10; subst.
+  specialize (Precede_Reserved _ _ _ H7 H1 H2).
   smack.
 - intros.
   inversion H; subst.
-  specialize (IHcards stacks' ResultStacks i H8).
+  specialize (Index_Increasing_True_Subset _ _ H0); smack.
+  destruct a.
+  inversion H0; subst.
+  specialize (Stacks_Indexes_Lt_Trans _ _ _ _ _ H7 H1 H9).
+  intros HZ1.
+  specialize (IHcards _ _ _ _ H10 H5 HZ1).
   apply IHcards; clear IHcards; smack.
-  admit.
-
-
-  .
-  
-Qed.
-
-(*
-Lemma lemma1: forall cards InitialStacks ResultStacks i stack_i stack_i_minus_1,
-  PatienceSolitaire_Rule cards InitialStacks ResultStacks ->
-  (* requirement for the initial stack *)
-  (forall j, j >= 2 -> j <= length InitialStacks -> 
-             Precede (nth j ResultStacks default) (nth (j - 1) ResultStacks default)) ->
-  i >= 2 -> 
-  i <= length ResultStacks ->
-  nth i ResultStacks default = stack_i ->
-  nth (i-1) ResultStacks default = stack_i_minus_1 ->
-  Precede stack_i stack_i_minus_1.
-Proof.
-  induction cards.
-- intros.
-  inversion H; subst.
-  specialize (H0 i H1 H2).
-  smack.
-- intros.
-  smack.
-  inversion H; subst.
-  
-  specialize (H0 i H)
-  
-  
-  
-  
+  specialize (Precede_Reserved _ _ _ H7 H1 H2); smack.
 Qed.
 
 
-Lemma lemma1: forall cards ResultStacks i stack_i stack_i_minus_1,
+(********************************************************************************
+   < Lemma 1 >
+   
+   To each element in stack i+ 1, there is an element in stack i that precedes it, 
+   so that by chaining these elements, we can construct an increasing subsequence 
+   of length n;
+ ********************************************************************************)
+
+
+Lemma lemma1: forall cards ResultStacks,
   PatienceSolitaire_Rule cards EmptyStacks ResultStacks ->
-  i >= 2 /\ i <= length ResultStacks ->
-  nth i ResultStacks default = stack_i ->
-  nth (i-1) ResultStacks default = stack_i_minus_1 ->
-  Precede stack_i stack_i_minus_1.
+  Index_Increasing_True cards ->
+  (forall i, i >= 1 -> 
+             i <= (length ResultStacks - 1) ->
+             Precede (nth (i-1) ResultStacks default) (nth i ResultStacks default)).
 Proof.
-  induction cards.
-- intros.
+  intros.
+  destruct cards.
+- inversion H; subst.
   smack.
-  inversion H; subst.
+- destruct p.
+  apply lemma1_helper with (cards := cards) (i := i0) (card := n) (InitialStacks := EmptyStacks);
   smack.
-- intros.
-  smack.
-  
+  constructor.
 Qed.
-*)
+
 
 
 (********************************************************************************
